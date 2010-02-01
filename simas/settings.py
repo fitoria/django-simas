588527e7cca9b94c32bfe14702b35f5254cba363
Adolfo Fitoria <fitoria@fitoria-laptop.(none)>
2010-02-01 12:17:19 -0600

    moving some stuff

--- simas/settings.py
+++ simas/settings.py
@@ -1,4 +1,4 @@
-# Django settings for django_simas project.
+# Django settings for simas project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -69,7 +69,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'django_simas.urls'
+ROOT_URLCONF = 'simas.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
